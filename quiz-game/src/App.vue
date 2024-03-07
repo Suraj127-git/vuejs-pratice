7aceb41d31d77a8803888ae3ff6a7af383751446
Suraj Shetty <shetty44444@gmail.com>
2024-03-08 01:58:35 +0530

    new project updated with mobile view

--- quiz-game/src/App.vue
+++ quiz-game/src/App.vue
@@ -1,30 +1,140 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+
+  <section class="score">
+    Player <span>{{ this.win_count }}</span> x 
+   <span>{{ this.lose_count }}</span> Computer
+  </section>
+
+  <template v-if="this.question">
+    <h1 v-html="this.question">
+    </h1>
+
+    <template v-for="(answer, index) in this.answers" v-bind:key="index">
+      <input 
+        :disabled="this.answerSubmitted" 
+        type="radio" name="options" 
+        :value="answer" 
+        v-model="this.chosen_answer">
+        
+      <label v-html="answer"></label><br>
+    </template>
+
+    <button v-if="!this.answerSubmitted" @click="this.submitAnswer()" class="send" type="button">Send</button>
+
+  </template>
+  
+
+  
+  <section class="result" v-if="this.answerSubmitted">
+    <template v-if="this.chosen_answer == this.correctAnswer">
+      <h4>&#9989; Congratulations, the answer "{{this.correctAnswer}}" is correct.</h4>
+    </template>
+    <template v-else>
+      <h4>&#10060;  I'm sorry, you picked the wrong answer. The correct is "{{this.correctAnswer}}".</h4>
+    </template>
+    <button @click="this.getNewQuestion()" class="send" type="button">Next question</button>
+  </section>
+
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script>
+
+export default {
+name: 'App',
+data() {
+  return {
+    chosen_answer: undefined,
+    question: undefined,
+    incorrectAnswers: [],
+    correctAnswer: '',
+    win_count: 0,
+    lose_count: 0,
+    answerSubmitted: false
+  }
+},
+computed: {
+    answers() {
+      var answers = JSON.parse(JSON.stringify(this.incorrectAnswers));
+      answers.splice(Math.round(Math.random() * answers.length), 0, this.correctAnswer);
+      return answers;
+    }
+},
+methods: {
+  submitAnswer() {
+    if (!this.chosen_answer) {
+      alert('Pick one of the options');
+    } else {
+      this.answerSubmitted = true;
+      if (this.chosen_answer == this.correctAnswer) {
+        this.win_count++;
+      } else {
+        this.lose_count++
+      }
+    }
+  },
+  getNewQuestion() {
+    this.chosen_answer = undefined;
+    this.answerSubmitted = false;
+    this. question = undefined;
+
+    this.axios
+    .get('https://opentdb.com/api.php?amount=1&category=30')
+    .then(response => (
+      this.question = response.data.results[0].question,
+      this.incorrectAnswers = response.data.results[0].incorrect_answers,
+      this.correctAnswer = response.data.results[0].correct_answer
+    ))
+  }
+},
+created() {
+
+  this.getNewQuestion();
+
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+</script>
+
+<style lang="scss">
+#app {
+font-family: Avenir, Helvetica, Arial, sans-serif;
+-webkit-font-smoothing: antialiased;
+-moz-osx-font-smoothing: grayscale;
+text-align: center;
+color: #2c3e50;
+margin: 60px auto;
+max-width: 960px;
+
+}
+
+h1 {
+margin-top: 40px;
+}
+
+input[type='radio']{
+margin: 12px 4px;
+}
+
+button.send {
+margin-top: 12px;
+height: 40px;
+min-width: 120px;
+padding: 0 16px;
+color: #fff;
+background-color: #1867c0;
+border: 1px solid #1867c0;
+cursor: pointer;
+}
+
+section.score {
+border-bottom: 1px solid black;
+padding: 24px;
+font-size: 18px;
+
+span {
+  padding: 8px;
+  font-weight: bold;
+  border: 1px solid black;
+}
 }
 </style>
